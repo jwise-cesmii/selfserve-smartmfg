30d3251eadb981992df9d854db044a5291b42f3e
jwise-cesmii <jonathan.wise@cesmii.org>
2020-02-27 17:52:53 -0500

    add some functions

--- gqlthinkiq.py
+++ gqlthinkiq.py
@@ -1,8 +1,51 @@
 from graphql_client import GraphQLClient
+from time import gmtime, strftime
 
 client = GraphQLClient('https://gql.annualmeeting.cesmii.thinkiq.net/graphql')
 
-def sendFridgeDoorSample(value, utcdatetime):
+def makeutcdatetime()
+  timestamp = strftime("%Y-%m-%dT%H:%M:%S", gmtime())
+  timestamp = timestamp + "-05:00"
+  return timestamp 
+
+def sendFridgeDoorSample(value):
+  utcdatetime = makeutcdatetime()
+  result = client.query('''
+  mutation UpdateFridgeData {
+    replaceTimeSeriesRange(
+      input: {
+        entries: [
+          {value: "''' + value + '''", timestamp: "''' + utcdatetime + '''", status: "0"}
+        ],
+        tagId: "1847"
+      }) {
+      string
+    }
+  }
+  ''')
+  print("ThinkIQ Mutate:")
+  print(str(result))
+
+def sendFridgeTemperatureSample(value):
+  utcdatetime = makeutcdatetime()
+  result = client.query('''
+  mutation UpdateFridgeData {
+    replaceTimeSeriesRange(
+      input: {
+        entries: [
+          {value: "''' + value + '''", timestamp: "''' + utcdatetime + '''", status: "0"}
+        ],
+        tagId: "1847"
+      }) {
+      string
+    }
+  }
+  ''')
+  print("ThinkIQ Mutate:")
+  print(str(result))
+
+  def sendFridgeHumiditySample(value):
+    utcdatetime = makeutcdatetime()
   result = client.query('''
   mutation UpdateFridgeData {
     replaceTimeSeriesRange(
